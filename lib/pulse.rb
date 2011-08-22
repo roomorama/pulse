8847563abc5a1cad1a250f6bf898addb4b108504
Donald Piret <donald@donaldpiret.com>
2011-08-23 04:57:32 +0800

    Disable loading of additional routes

--- lib/pulse.rb
+++ lib/pulse.rb
@@ -1,3 +1,3 @@
 require File.dirname(__FILE__) + '/pulse_controller'
 require File.dirname(__FILE__) + '/pulse_helper'
-require File.dirname(__FILE__) + '/routes'
+#require File.dirname(__FILE__) + '/routes'
